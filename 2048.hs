c609fa886b97db50f021e69d0ed9f2e80c10b0dc
Meowcolm024 <luoyicong1026@live.com>
2020-04-25 10:39:33 +0800

    update

--- 2048.hs
+++ 2048.hs
@@ -4,57 +4,55 @@ import           Data.List                      ( transpose, group )
 import           System.Random                  ( randomRIO )
 import           Control.Monad                  ( replicateM )
 
-data Grid = Grid [[Int]] deriving (Show, Eq)
+type Grid = [[Int]]
 data Act = Ls | Rs | Us | Ds deriving (Show, Eq)
 
 main :: IO ()
-main = gen (Grid $ chunksOf 4 $ replicate 16 0) >>= loop
+main = gen (chunksOf 4 $ replicate 16 0) >>= loop
 
 loop :: Grid -> IO ()
 loop x = do
-  showScore x
-  showGrid x
+  showScore x >> showGrid x
   putStr "\nType in the control (WASD): "
-  cmd <- getLine
-  gen (move (act $ head cmd) x) >>= loop
+  getLine >>= \y -> let a = act . head $ y ++ " " in case a of
+    Just a' -> gen (move a' x) >>= loop
+    Nothing -> loop x
 
 showGrid :: Grid -> IO ()
-showGrid (Grid x) = mapM_ (putStrLn . f) x where
+showGrid = mapM_ (putStrLn . f) where
   f m = unwords $ map (\y -> if y == 0 then "_" else show y) m
 
 showScore :: Grid -> IO ()
-showScore (Grid x) = do
+showScore x = do
   putStrLn "--------------------------"
   putStr   $  "Total: " ++ (show . sum . concat) x 
   putStrLn $ " Max: "  ++ (show . maximum . concat) x
   putStrLn "--------------------------"
 
 step :: [Int] -> [Int]
-step x = y ++ replicate (4 - length y) 0
- where
+step x = y ++ replicate (4 - length y) 0 where
   y = map sum . concatMap (chunksOf 2) . group . filter (/= 0) $ x
 
 move :: Act -> Grid -> Grid
-move d (Grid x) = case d of
-  Ls -> Grid $ map step x
-  Rs -> Grid $ map (reverse . step . reverse) x
-  Us -> Grid $ transpose . map step . transpose $ x
-  Ds -> Grid $ transpose . map (reverse . step . reverse) . transpose $ x
+move d x = case d of
+  Ls -> map step x
+  Rs -> map (reverse . step . reverse) x
+  Us -> transpose . map step . transpose $ x
+  Ds -> transpose . map (reverse . step . reverse) . transpose $ x
 
 gen :: Grid -> IO Grid
-gen (Grid g) = do
-  r <-
-    replicateM 16
+gen g = do
+  r <- replicateM 16
     $   (\x y z -> x * y * z * 2)
     <$> randomRIO (0, 1)
     <*> randomRIO (0, 1)
     <*> randomRIO (0, 2)
-  return $ Grid $ chunksOf 4 
-                $ zipWith (\x y -> if x == 0 then y else x) (concat g) r
+  return $ chunksOf 4 $ zipWith (\x y -> if x == 0 then y else x) (concat g) r
 
-act :: Char -> Act
+act :: Char -> Maybe Act
 act x = case x of
-  'a' -> Ls
-  'd' -> Rs
-  'w' -> Us
-  's' -> Ds
+  'a' -> Just Ls
+  'd' -> Just Rs
+  'w' -> Just Us
+  's' -> Just Ds
+  _   -> Nothing
